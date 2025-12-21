4d5bf9d8054edabb7a19b45abd16c0b01d905b7d
Sadman Al Sakib <150785896+saadmanshakiib@users.noreply.github.com>
2025-12-21 02:49:57 -0800

    Add files via upload

--- src/AdminPanel/AddRestaurant.jsx
+++ src/AdminPanel/AddRestaurant.jsx
@@ -1,8 +1,48 @@
+import {useRef} from "react";
+import '/src/AdminPanel/AddRestaurant.css';
 const AddRestaurant=()=>{
-    return(
-        <div>
+
+    const restaurantNameRef = useRef();
+
+    const handleAdd = async()=>{
+       const response =  await fetch("http://localhost:1234/addrestaurants",{
+            method : "POST",
+            credentials : "include",
+            headers : {"content-type" : "application/json"},
+            body : JSON.stringify({
+                name: restaurantNameRef.current.value
+            })
+        })
+
+       if(response.status === 200){
+           alert("Restaurant added successfully.");
+       }
+       else{
+           alert(response.message);
+       }
+    }
+
+
+    return (
+        <div className="add-restaurant">
+            <div className="add-restaurant-box">
+                <div className="add-restaurant-inputs">
+                    <input
+                        type="text"
+                        placeholder="Restaurant name"
+                        ref={restaurantNameRef}
+                    />
+                    <button
+                        className="addRestaurantButton"
+                        onClick={handleAdd}
+                    >
+                        Add Restaurant
+                    </button>
+                </div>
+            </div>
         </div>
-    )
+    );
+
 
 }
 export default AddRestaurant;
